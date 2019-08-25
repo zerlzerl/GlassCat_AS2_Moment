22062ec95fc87ed577cb1bcfacf60e87185e2bfc
e <zerlzerl@163.com>
2019-08-25 12:59:26 +0800

    register complete

--- GlassCat_AS2_Moment/Login.aspx.cs
+++ GlassCat_AS2_Moment/Login.aspx.cs
@@ -23,60 +23,23 @@ namespace GlassCat_AS2_Moment.Pages
 
         protected void ValidateUser(object sender, EventArgs e)
         {
-            // after customer validation, user is validated, username can be used in form authentication
-            string username = loginUsername.Text;
-            FormsAuthentication.SetAuthCookie(username, true);
-            string retrnUrl = Request.QueryString["returnUrl"];
-            if (!string.IsNullOrEmpty(retrnUrl))
+            if (Page.IsValid)
             {
-                //Redirect to Original requested page
-                FormsAuthentication.RedirectFromLoginPage(username, rememberCheckbox.Checked);
-            }
-            else
-            {
-                //If user directly opened login page, always show him Homepage.
-                Response.Redirect("/");
-            }
-
-            //string username = txtUsername.Text.Trim();
-            //string password = txtPassword.Text.Trim();
-            //int userId = 0;
-            //string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
-            //using (SqlConnection con = new SqlConnection(constr))
-            //{
-            //    using (SqlCommand cmd = new SqlCommand("Validate_User"))
-            //    {
-            //        cmd.CommandType = CommandType.StoredProcedure;
-            //        cmd.Parameters.AddWithValue("@Username", username);
-            //        cmd.Parameters.AddWithValue("@Password", password);
-            //        cmd.Connection = con;
-            //        con.Open();
-            //        userId = Convert.ToInt32(cmd.ExecuteScalar());
-            //        con.Close();
-            //    }
-            //    switch (userId)
-            //    {
-            //        case -1:
-            //            dvMessage.Visible = true;
-            //            lblMessage.Text = "Username and/or password is incorrect.";
-            //            break;
-            //        case -2:
-            //            dvMessage.Visible = true;
-            //            lblMessage.Text = "Account has not been activated.";
-            //            break;
-            //        default:
-            //            if (!string.IsNullOrEmpty(Request.QueryString["ReturnUrl"]))
-            //            {
-            //                FormsAuthentication.SetAuthCookie(username, chkRememberMe.Checked);
-            //                Response.Redirect(Request.QueryString["ReturnUrl"]);
-            //            }
-            //            else
-            //            {
-            //                FormsAuthentication.RedirectFromLoginPage(username, chkRememberMe.Checked);
-            //            }
-            //            break;
-            //    }
-            //}
+                // after customer validation, user is validated, username can be used in form authentication
+                string username = loginUsername.Text;
+                FormsAuthentication.SetAuthCookie(username, true);
+                string retrnUrl = Request.QueryString["returnUrl"];
+                if (!string.IsNullOrEmpty(retrnUrl))
+                {
+                    //Redirect to Original requested page
+                    FormsAuthentication.RedirectFromLoginPage(username, rememberCheckbox.Checked);
+                }
+                else
+                {
+                    //If user directly opened login page, always show him Homepage.
+                    Response.Redirect("/");
+                }
+            }            
         }
 
         protected void LoginValidator_ServerValidate(object source, ServerValidateEventArgs args)
