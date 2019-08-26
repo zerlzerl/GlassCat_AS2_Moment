11cc99b852c3170b2f5b589e4550effe481a8bf9
e <zerlzerl@163.com>
2019-08-26 15:41:41 +0800

    dynamic multi code display

--- GlassCat_AS2_Moment/AdditionalPages/DisplayCode.aspx.cs
+++ GlassCat_AS2_Moment/AdditionalPages/DisplayCode.aspx.cs
@@ -6,30 +6,58 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.IO;
 using System.Text;
+using System.Collections;
+
 namespace GlassCat_AS2_Moment.AdditionalPages
-{
+{   
     public partial class DisplayCode : System.Web.UI.Page
     {
+        public partial class CodeObject
+        {
+            public string filename;
+            public string code;
+
+            public CodeObject()
+            {
+            }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            string filePath = Server.MapPath(Request.QueryString["filename"]);
-            FileName.Text = Request.QueryString["filename"];
-            if (!File.Exists(filePath))
+            string filenames = Request.QueryString["filenames"];
+            string[] filenameList = new string[1];
+            if (string.IsNullOrEmpty(filenames))
             {
-                Code.Text = "File not exists.";
-                return;
+                filenameList[0] = "";
             }
-
-            FileInfo file = new FileInfo(filePath);
-            if (file.Extension == ".mdb" || file.Extension == ".exe" || file.Extension == ".accdb"
-                || file.Extension == ".docx" || file.Extension == ".xlsx" || file.Extension == ".jpg"
-                || file.Extension == ".png")
+            else
             {
-                Code.Text = "Sorry you cannot read a file with an extension of " + file.Extension;
-                return;
+                filenameList = filenames.Split(';');
             }
+            ArrayList CodeList = new ArrayList();
+            foreach(string filename in filenameList) {
+                CodeObject code = new CodeObject();
+                string filePath = Server.MapPath(filename);
+                code.filename = filename;
+                if (!File.Exists(filePath))
+                {
+                    code.code = "File not exists.";
+                    return;
+                }
 
-            Code.Text = ReadFile(filePath);
+                FileInfo file = new FileInfo(filePath);
+                if (file.Extension == ".mdb" || file.Extension == ".exe" || file.Extension == ".accdb"
+                    || file.Extension == ".docx" || file.Extension == ".xlsx" || file.Extension == ".jpg"
+                    || file.Extension == ".png")
+                {
+                    code.code = "Sorry you cannot read a file with an extension of " + file.Extension;
+                    return;
+                }
+                code.code = ReadFile(filePath);
+                CodeList.Add(code);
+            }
+            CodesReqeater.DataSource = CodeList;
+            CodesReqeater.DataBind();
         }
 
         private string ReadFile(string filePath)
